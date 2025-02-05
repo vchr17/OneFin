baf363ddd750b4461456933d5bed831d07fe6b8e
Nikita <vecherski.ni@gmail.com>
2025-02-05 15:38:09 +0300

    done splash&onboarding screens

--- app/src/main/java/com/example/onefin/presentation/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/onefin/presentation/activities/SplashScreenActivity.kt
@@ -1,34 +1,40 @@
 package com.example.onefin.presentation.activities
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.onefin.R
-import com.example.onefin.databinding.ActivityMainBinding
-import com.example.onefin.presentation.fragments.FirstOnboardingFragment
+import com.example.onefin.databinding.ActivitySplashScreenBinding
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
 
-    private var binding : ActivityMainBinding? = null
+    private var binding : ActivitySplashScreenBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        if(savedInstanceState == null){
-            supportFragmentManager.beginTransaction().add(R.id.fragment_container,
-                FirstOnboardingFragment(), "FirstOnboarding").commit()
-        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.fragmentContainer
+        binding.oneFinLogo.alpha = 0f
+        binding.oneFinLogo.animate().setDuration(1500).alpha(1f).withEndAction {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
     }
+
+
 
     override fun onDestroy() {
         binding = null
